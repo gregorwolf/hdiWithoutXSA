7fb0de109b061324e4d2e5f542ecc033bcfc7546
Gregor Wolf <gregor.wolf@gmail.com>
2019-11-19 22:03:00 +0100

    Correct system id

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Boostrap HDI Infrastructure"
-hdbsql -d HXE -u SYSTEM -n hanapm.local.com:39015 -i 90 -p $1 -A -m -j -V $1 -I bootstrap.sql
+hdbsql -d HXE -u SYSTEM -n hanapm.local.com:30015 -i 00 -p $1 -A -m -j -V $1 -I bootstrap.sql
