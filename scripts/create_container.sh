5535a0c2dd2ab15ab965ab9eecaf710eef0b246a
Gregor Wolf <gregor.wolf@gmail.com>
2019-11-19 22:04:05 +0100

    Correct message

--- scripts/create_container.sh
+++ scripts/create_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "Boostrap HDI Infrastructure"
+echo "Create HDI Container"
 hdbsql -d HXE -u HDI_ADMIN -n hanapm.local.com:30015 -i 00 -p $1 -A -m -j -V $1,$2 -I create_container.sql
